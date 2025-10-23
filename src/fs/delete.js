a6e4f93f7cf2076bd2800c742245ec2f47dd4295
etcq <resonans93@gmail.com>
2025-10-23 06:19:19 +0500

    fs task completed

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import { rm } from 'node:fs/promises';
+import { join } from 'node:path'
+
 const remove = async () => {
-  // Write your code here
+  try {
+    const path = await join(import.meta.dirname, './files/fileToRemove.txt');
+    await rm(path);
+  } catch {
+      throw new Error('FS operation failed');
+  }
 };
 
 await remove();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import * as fs from 'node:fs/promises';
+import { join } from 'node:path';
+
 const rename = async () => {
-  // Write your code here
+  try {
+      const path = import.meta.dirname
+      const oldNamePath = await join(import.meta.dirname, './files/wrongFilename.txt');
+      const newNamePath = await join(import.meta.dirname, './files/properFilename.md');
+      await fs.rename(oldNamePath, newNamePath);
+  } catch {
+    throw new Error('FS operation failed');
+  } 
+
 };
 
 await rename();
