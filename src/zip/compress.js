9c373ccc31dbfb5da98e72ebd8cbc5883dcb58c7
etcq <resonans93@gmail.com>
2025-10-26 00:55:50 +0500

    zip tasks completed

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import {
+  createReadStream,
+  createWriteStream,
+} from 'node:fs';
+import  { createGzip } from 'node:zlib';
+import { join } from 'path';
+
 const compress = async () => {
-  // Write your code here
+  const gzip = createGzip();
+  const source = createReadStream(join(import.meta.dirname, './files/fileToCompress.txt'));
+  const destination = createWriteStream(join(import.meta.dirname, './files/archive.gz'));
+  source.pipe(gzip).pipe(destination);
 };
 
 await compress();
