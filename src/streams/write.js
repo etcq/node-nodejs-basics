034fd5bfff1db9b5536004f952dc9c76c3dd1390
etcq <resonans93@gmail.com>
2025-10-26 00:43:29 +0500

    streams tasks completed

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,10 @@
+import { createWriteStream } from 'fs';
+import { stdin } from 'process';
+import { join } from 'path';
+
 const write = async () => {
-  // Write your code here
+  const output = createWriteStream(join(import.meta.dirname, './files/fileToWrite.txt'));
+  stdin.pipe(output);
 };
 
 await write();
